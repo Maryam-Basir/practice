c1e982edc05c0f8e8a58b5d49e5f4c65664e2f01
maryambasir992 <jobbasirabyaneh@gmail.com>
2023-09-09 09:47:33 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function sum(){
+let number1=document.getElementById("num1").value;
+let number2=document.getElementById("num2").value;
+let result=Number(number1)+Number(number2);
+document.getElementById("text").textContent=result;
+}
